265021112ee926ad889b3b7e3f249e7df5cf91e7
impriyaranjan <54205207+impriyaranjan@users.noreply.github.com>
2022-03-11 21:54:16 +0530

    Updates

---
+++ precache-manifest.84dc6de8c088765d1078cd140db386dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaccb536fa29685bea585222b4944dd",
+    "url": "/impriyaranjan/amazon-clone/index.html"
+  },
+  {
+    "revision": "7f1816e9f7401f1a69e6",
+    "url": "/impriyaranjan/amazon-clone/static/css/main.ca7501fa.chunk.css"
+  },
+  {
+    "revision": "136af0836f8d8dc10f0e",
+    "url": "/impriyaranjan/amazon-clone/static/js/2.ae82c13b.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/impriyaranjan/amazon-clone/static/js/2.ae82c13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1816e9f7401f1a69e6",
+    "url": "/impriyaranjan/amazon-clone/static/js/main.5cec46e8.chunk.js"
+  },
+  {
+    "revision": "6fcc06c7cfe169c26111",
+    "url": "/impriyaranjan/amazon-clone/static/js/runtime-main.5589ce97.js"
+  }
+]);
